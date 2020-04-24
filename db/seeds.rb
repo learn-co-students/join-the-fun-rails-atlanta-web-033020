1737cd731e7b74238c8f1b03bdee49cdeef1eb1d
github username <github email address>
2020-04-24 16:20:14 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Ride.destroy_all
+Taxi.destroy_all
+Passenger.destroy_all
+
+brandon = Passenger.create
+ashley = Passenger.create
+drew = Passenger.create
+
+taxi1 = Taxi.create
+taxi2 = Taxi.create
+taxi3 = Taxi.create
+taxi4 = Taxi.create
+
+ride1 = Ride.create(passenger_id: brandon.id, taxi_id: taxi1.id)
+ride2 = Ride.create(passenger_id: ashley.id, taxi_id: taxi2.id)
+ride3 = Ride.create(passenger_id: drew.id, taxi_id: taxi3.id)
+ride4 = Ride.create(passenger_id: brandon.id, taxi_id: taxi4.id)
+ride5 = Ride.create(passenger_id: ashley.id, taxi_id: taxi1.id)
+ride6 = Ride.create(passenger_id: drew.id, taxi_id: taxi2.id)
